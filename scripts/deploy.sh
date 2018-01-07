d11831145019760b98e19584102424c89ac7b421
lcxfs1991 <lcxfs1991@gmail.com>
2018-01-07 11:56:02 +0800

    try to update gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 SOURCE_BRANCH="cn"
 MASTER_BRANCH="master"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$TRAVIS_BRANCH" != "$MASTER_BRANCH"]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$TRAVIS_BRANCH" != "$MASTER_BRANCH" ]; then
     echo "Skipping deploy; just doing a build"
     npm run build
     exit 0
